e101d3a93d4d4bd9ca4409d722ea8fc7f55f27f4
Hridayesh Kumar Gupta <hridayesh.gupta@mindtickle.com>
2015-05-23 16:42:06 +0530

    fix code. now its runnabe

--- script/service.js
+++ script/service.js
@@ -5,16 +5,16 @@
 angular.module("randomApp")
 .service("RandomService", ["$resource", function($resource) {
 	return $resource("/", {}, {
-
+/*
 		getSuggestions: {url:"/suggestios.php", method:"GET", isArray:true},
 		getResults:{url:"/results.php", method:"GET", isArray:true},
 		fetchFacets: {url:"/facets.php", method:"GET", isArray:true},
 		fetchInfo: {url:"/info.php", method:"GET"}
-
-		/*getSuggestions: {url:"/suggestios/:term", method:"GET", isArray:true},
+*/
+		getSuggestions: {url:"/suggestios/:term", method:"GET", isArray:true},
 		getResults:{url:"/results/:term", method:"GET", isArray:true},
 		fetchFacets: {url:"/facets", method:"GET", isArray:true},
-		fetchInfo: {url:"/info/:id", method:"GET"}*/
+		fetchInfo: {url:"/info/:id", method:"GET"}
 
 	});
 }]);
