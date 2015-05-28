6f799da9683ae3bf2868945f92a7aa7f63b58595
gangele397 <gangele397@gmail.com>
2015-05-28 18:48:25 +0530

    added file for suggestion
    added file for suggestion

---
+++ results_new.php
@@ -0,0 +1,44 @@
+{"facets": ["mindtickle", "ola", "jeetu"],
+"list":[
+	{
+		"id": 12,
+		"TopTopics":["topic1", "topic1", "topic1"],
+		
+		"SemanticTags":["topic1", "topic1", "topic1"],
+		"name": "Insideview",
+		"logo":"http://lorempixel.com/400/200/sports/1/"
+	},
+	{
+		"id": 12,
+		"TopTopics":["topic1", "topic1", "topic1"],
+		
+		"SemanticTags":["topic1", "topic1", "topic1"],
+		"name": "Insideview",
+		"logo":"http://lorempixel.com/400/200/sports/1/"
+	},
+	{
+		"id": 12,
+		"TopTopics":["topic1", "topic1", "topic1"],
+		
+		"SemanticTags":["topic1", "topic1", "topic1"],
+		"name": "Insideview",
+		"logo":"http://lorempixel.com/400/200/sports/1/"
+	},
+	{
+		"id": 12,
+		"TopTopics":["topic1", "topic1", "topic1"],
+		
+		"SemanticTags":["topic1", "topic1", "topic1"],
+		"name": "Insideview",
+		"logo":"http://lorempixel.com/400/200/sports/1/"
+	},
+	{
+		"id": 12,
+		"TopTopics":["topic1", "topic1", "topic1"],
+		
+		"SemanticTags":["topic1", "topic1", "topic1"],
+		"name": "Insideview",
+		"logo":"http://lorempixel.com/400/200/sports/1/"
+	}
+]
+}
