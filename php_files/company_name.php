ef71cf7666c428ae8c0014311fe8f70d4c324eec
Hridayesh Kumar Gupta <hridayesh.gupta@mindtickle.com>
2015-05-29 09:13:42 +0530

    search

--- php_files/company_name.php
+++ php_files/company_name.php
@@ -27,7 +27,7 @@ error_reporting(~E_ALL );
 		
 	}
 }
-	$companyData= getCompanyInfodata("Microsoft");
+	$companyData= getCompanyInfodata($_GET["query"]);
 	echo $jsonformat=json_encode($companyData);
 	//foreach($companyData as $x => $x_value) {
 	//	echo $x."=".$x_value;
--- script/service.js
+++ script/service.js
@@ -7,7 +7,7 @@ angular.module("randomApp")
 	return $resource("/", {}, {
 
 		getSuggestions: {url:"/suggestios.php", method:"GET", isArray:true},
-		getResults:{url:"/TextFile_output/Search.txt", method:"GET"},
+		getResults:{url:"/php_files/company_name.php", method:"GET"},
 		fetchFacets: {url:"/facets.php", method:"GET", isArray:true},
 		fetchInfo: {url:"/info.php", method:"GET"},
 		fetchTopic: {url:"/php_files/SearchArticleWithCompanyIDandTopic.php", method:"GET"}
